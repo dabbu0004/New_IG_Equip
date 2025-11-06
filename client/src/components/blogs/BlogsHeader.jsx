66d455729f3c2706bb54c1e5d74ad610de45b12a
Nouman Khan <noumankhan16859@gmail.com>
2025-11-06 15:30:36 +0530

    New: Blogs file

---
+++ client/src/components/blogs/BlogsHeader.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const BlogsHeader = () => {
+  return (
+    <div>BlogsHeader</div>
+  )
+}
+
+export default BlogsHeader
---
+++ client/src/pages/BlogsPage.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import BlogsHeader from "../components/blogs/BlogsHeader";
+import BlogsMain from "../components/blogs/BlogsMain";
+
+const BlogsPage = () => {
+  return (
+    <div>
+      <BlogsHeader />
+      <BlogsMain />
+    </div>
+  );
+};
+
+export default BlogsPage;
